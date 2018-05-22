57f3284a6bbca106dd957adfb3c3090c80bcc892
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2018-05-22 10:11:15 -0500

    Funcpacking 📦

--- .funcpack/original.index.js
+++ .funcpack/original.index.js
@@ -1,5 +1,4 @@
 module.exports = {
     "getConnectionInfo": require("../getConnectionInfo/index.js"),
-    "heartRateUpdated": require("../heartRateUpdated/index.js"),
     "updateHeartRate": require("../updateHeartRate/index.js")
 }
